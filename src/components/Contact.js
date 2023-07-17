204830fb9110b3025cfc26bf5b752e014ce05e01
AyushSaxena092000 <ayushsaxena092000@gmail.com>
2023-07-17 16:17:08 +0530

    emailadded

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 import { Link } from "react-scroll";
 
 const Contact = () => {
+  const formRef = useRef();
   const [email, setEmail] = useState("");
   const [isEmailValid, setEmailValid] = useState(true);
-
-  const validateEmail = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     setEmailValid(emailRegex.test(email));
-  };
-  const handleClick = () => {
-    validateEmail();
 
-    window.alert(
-      "We apologize for the inconvenience. Our website is currently undergoing maintenance. Please check back later!"
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+
+  const handleClick = (e) => {
+    handleChange(e);
+    e.preventDefault();
+    console.log('form', form)
+
+    emailjs
+    .send(
+      'service_z068fa9',
+      'template_w4dug38',
+      {
+        from_name: form.name,
+        to_name: "ayush",
+        from_email: form.email,
+        to_email: "ayushsaxena452@gmail.com",
+        message: form.message,
+      },
+      'DUByFhMOx8YVtZrgi'
+    )
+    .then(
+      () => {
+        setLoading(false);
+        alert("Thank you. I will get back to you as soon as possible.");
+
+        setForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      },
+      (error) => {
+        setLoading(false);
+        console.error(error);
+
+        alert("Ahh, something went wrong. Please try again.");
+      }
     );
   };
   return (
@@ -44,20 +90,27 @@ const Contact = () => {
             variants={fadeIn("left", 0.3)}
             initial="hidden"
             whileInView={"show"}
+            ref={formRef}
+            onSubmit={handleClick} 
             viewport={{ once: false, amount: 0.3 }}
             className="flex-1 border rounded-2xl flex-col gap-y-6 pb-24 p-6 items-start"
           >
             <input
-              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
-              type="text"
-              placeholder="Your name"
+           type="text"
+           name="name"
+           value={form.name}
+           onChange={handleChange}
+           placeholder="What's your good name?"
+           className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
             />
             <input
-              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
-              type="text"
-              placeholder="Your email"
-              onChange={(e) => setEmail(e.target.value)}
-              onBlur={validateEmail}
+             type="email"
+             name="email"
+             value={form.email}
+             onChange={handleChange}
+             placeholder="What's your web address?"
+             className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+            
             />
             {!isEmailValid && (
               <p style={{ color: "red" }}>
@@ -65,11 +118,15 @@ const Contact = () => {
               </p>
             )}
             <textarea
-              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
-              placeholder="Your message"
+              //  rows={7}
+               name="message"
+               value={form.message}
+               onChange={handleChange}
+               placeholder="What you want to say?"
+               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"
             ></textarea>
             <Link to="contact">
-              <button className="btn btn-lg" onClick={handleClick}>
+              <button className="btn btn-lg" onClick={handleClick} {...loading ? "Sending..." : "Send"}>
                 Send message
               </button>
             </Link>
